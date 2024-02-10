7d2bfb0ffe13fac028639f29845b33cbbc452b7f
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2024-02-10 17:38:03 -0300

    verifyng instance of error to provide a better error understanding

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -3,12 +3,18 @@ This module contains the error handler to handle the exceptions.
 """
 
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 def handle_errors(error: Exception) -> HttpResponse:
     """
     This method is used to handle the exceptions.
     """
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={"errors": [{"title": error.name, "detail": str(error)}]},
+        )
     return HttpResponse(
         status_code=500,
         body={"errors": [{"title": "Internal Server Error", "detail": str(error)}]},
